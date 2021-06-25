2fea555f4b1425a732b8cc59590481399eaf9371
Bhupendra <bhupendra1011@gmail.com>
2021-06-25 22:25:52 +0530

    saving user to DB from cognito

--- App.tsx
+++ App.tsx
@@ -1,16 +1,19 @@
 import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
 import VideoScreen from './screens/VideoScreen';
-import Amplify from 'aws-amplify'
+import Amplify, { Auth, DataStore } from 'aws-amplify'
 import config from './src/aws-exports'
 
+import { User } from './src/models';
+
 import { withAuthenticator } from "aws-amplify-react-native";
+
 
 Amplify.configure(config)
 
@@ -18,6 +21,40 @@ Amplify.configure(config)
 function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = "dark";//useColorScheme();
+
+
+
+  // sync user from cognito to amplify DataStore
+  // once user is logged in 
+  useEffect(() => {
+
+    const saveUserToDB = async () => {
+      // get user form cognitio
+      const userInfo = await Auth.currentAuthenticatedUser();
+      if (!userInfo) return;
+
+      const userId = userInfo.attributes.sub;
+      console.log(userId);
+
+      // check if it exits in DB , if not save in DB
+      const user = (await DataStore.query(User)).find(user => user.sub === userId);
+      if (!user) {
+        //save to DB
+        await DataStore.save(new User({
+          sub: userId,
+          name: userInfo.attributes.email,
+          subscribers: 0
+        }))
+
+      } else {
+        console.log("user exists in DB already")
+      }
+
+    };
+    saveUserToDB();
+
+
+  }, [])
 
   if (!isLoadingComplete) {
     return null;
--- src/models/schema.js
+++ src/models/schema.js
@@ -71,6 +71,13 @@ export const schema = {
                         "targetName": "commentVideoId"
                     }
                 },
+                "userID": {
+                    "name": "userID",
+                    "isArray": false,
+                    "type": "ID",
+                    "isRequired": false,
+                    "attributes": []
+                },
                 "createdAt": {
                     "name": "createdAt",
                     "isArray": false,
@@ -101,6 +108,15 @@ export const schema = {
                         "name": "byVideo",
                         "fields": [
                             "videoID"
+                        ]
+                    }
+                },
+                {
+                    "type": "key",
+                    "properties": {
+                        "name": "byUser",
+                        "fields": [
+                            "userID"
                         ]
                     }
                 },
@@ -176,6 +192,27 @@ export const schema = {
                         "connectionType": "HAS_MANY",
                         "associatedWith": "userID"
                     }
+                },
+                "Comments": {
+                    "name": "Comments",
+                    "isArray": true,
+                    "type": {
+                        "model": "Comment"
+                    },
+                    "isRequired": false,
+                    "attributes": [],
+                    "isArrayNullable": true,
+                    "association": {
+                        "connectionType": "HAS_MANY",
+                        "associatedWith": "userID"
+                    }
+                },
+                "sub": {
+                    "name": "sub",
+                    "isArray": false,
+                    "type": "String",
+                    "isRequired": false,
+                    "attributes": []
                 },
                 "createdAt": {
                     "name": "createdAt",
@@ -393,5 +430,5 @@ export const schema = {
     },
     "enums": {},
     "nonModels": {},
-    "version": "b8873a28179e4f1b3112b36ccf306193"
+    "version": "a4b7228bb5c0f221ed1a72e7ab837506"
 };
